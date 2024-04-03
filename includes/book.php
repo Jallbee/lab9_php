fed1c7539d054b837efd79f50a632dd18a13b2e3
Jallbee <158083923+Jallbee@users.noreply.github.com>
2024-04-03 12:44:11 -0400

    com

--- includes/book.php
+++ includes/book.php
@@ -19,3 +19,48 @@ $hunger_games = new Book ('The Hunger Games', 'Suzanne Collins', 5,14.99,1);
 $catching_fire =new Book ('Catching Fire', 'Suzanne Collins',4,3.99,3);
 $mockingjay - new Book ('Mockingjay', 'Suzanne Collins',3,10.99,5)
 ?>
+
+<?php
+
+
+class Book {
+    public string $title;
+    public string $author;
+    public int $rating;
+    public float $price;
+    public int $quantity;
+   
+    public function __construct(string $title, string $author, int $rating, float $price, int $quantity) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->rating = $rating;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+   
+    public function checkInStock() {
+        $message = '';
+        if ($this->quantity > 0) {
+            $message = 'In Stock';
+        }
+        else {
+            $message = 'Out of Stock';
+        }
+        return $message;
+    }
+
+
+    public function buy() {
+        $this->quantity = $this->quantity - 1;
+        return $this->quantity;
+    }
+
+
+    public function showRatingStars() {
+        $star = '';
+        for ($i = 0; $i < $this->rating; $i++)  {
+            $star .= '<span class="fa fa-star checked"></span>';
+        }
+        for ($i = $this->rating; $i < 5; $i++)  {
+            $star .= '<span class="fa fa-star"></span>';
+        }
--- index.php
+++ index.php
@@ -1,18 +1,83 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html>
-	<head>
-		<title>Lab 9: The Bookworm Store</title>
-		<link rel="stylesheet" href="css/bookworm-style.css" />
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	</head>
+    <head>
+        <title>Lab 9: The Bookworm Store</title>
+        <link rel="stylesheet" href="css/bookworm-style.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
 
-	<body>
-		<header>
-			<h1>THE BOOKWORM STORE</h1>
-			<nav>
-				<a href="index.php">HOME</a>
-			</nav>
-    	</header>
+
+    <body>
+        <header>
+            <h1>THE BOOKWORM STORE</h1>
+            <nav>
+                <a href="index.php">HOME</a>
+            </nav>
+        </header>
+       <!DOCTYPE html>
+<html>
+    <head>
+        <title>Lab 9: The Bookworm Store</title>
+        <link rel="stylesheet" href="css/bookworm-style.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+
+    <body>
+        <header>
+            <h1>THE BOOKWORM STORE</h1>
+            <nav>
+                <a href="index.php">HOME</a>
+            </nav>
+        </header>
+        <?php include 'includes/book.php'; ?>
+        <?php $hunger_games->buy() ?>
+        <section>
+            <div>
+                <h2>The BookWorm Store</h2>
+                <h4>I'm not a <span class="emphasized">bookworm</span>, I'm an <span class="emphasized">escape artist</span>.</h4>
+            </div>
+
+
+            <h4 class="decorated"><span><span class="emphasized">The Hunger Games</span></span></h4>
+
+
+            <div id="content">
+                <div class="row">
+
+
+                    <div class="column">
+                        <img src="images/hunger-games.jpeg" style="width:60%">
+                        <h5><?= $hunger_games->title ?></h5>
+                        <h5><?= $hunger_games->author ?></h5>
+                        <h5><?= $hunger_games->showRatingStars() ?></h5>
+                        <h5><?= $hunger_games->checkInStock() ?></h5>
+                        <h5>$<?= $hunger_games->price ?></h5>
+                    </div>
+
+
+                    <div class="column">
+                        <img src="images/catching-fire.jpeg" style="width:60%">
+                        <h5><?= $catching_fire->title ?></h5>
+                        <h5><?= $catching_fire->author ?></h5>
+                        <h5><?= $catching_fire->showRatingStars() ?></h5>
+                        <h5><?= $catching_fire->checkInStock() ?></h5>
+                        <h5>$<?= $catching_fire->price ?></h5>
+                    </div>
+
+
+                    <div class="column">
+                        <img src="images/mockingjay.jpeg" style="width:60%">
+                        <h5><?= $mockingjay->title ?></h5>
+                        <h5><?= $mockingjay->author ?></h5>
+                        <h5><?= $mockingjay->showRatingStars() ?></h5>
+                        <h5><?= $mockingjay->checkInStock() ?></h5>
+                        <h5>$<?= $mockingjay->price ?></h5>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
 
     	<section>
       		<div>

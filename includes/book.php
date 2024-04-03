e3e2877f84e6c351bf47938725c71fc942546446
Jallbee <158083923+Jallbee@users.noreply.github.com>
2024-04-03 12:48:54 -0400

    commit

--- includes/book.php
+++ includes/book.php
@@ -1,25 +1,3 @@
-<? php
-class Book {
-	public string $title ;
-	public string $author;
-	public int $ratings;
-	public float $price;
-	public int $quantity;
-
-	public function_construct(string $title, string $author, int $ratings,Float $price,int $quantity){
-		$this -> title= $title;
-		$this -> author= $author;
-		$this -> ratings= $ratings;
-		$this -> price= $price;
-		$this -> quantity= $quantity;
-	}
-}
-
-$hunger_games = new Book ('The Hunger Games', 'Suzanne Collins', 5,14.99,1);
-$catching_fire =new Book ('Catching Fire', 'Suzanne Collins',4,3.99,3);
-$mockingjay - new Book ('Mockingjay', 'Suzanne Collins',3,10.99,5)
-?>
-
 <?php
 
 
@@ -64,3 +42,12 @@ class Book {
         for ($i = $this->rating; $i < 5; $i++)  {
             $star .= '<span class="fa fa-star"></span>';
         }
+return $star;
+    }
+}
+
+
+$hunger_games = new Book('The Hunger Games', 'Suzanne Collins', 5, 14.99, 1);
+$catching_fire = new Book('Catching Fire', 'Suzanne Collins', 4, 13.99, 3);
+$mockingjay = new Book('Mockingjay', 'Suzanne Collins', 3, 10.99, 5);
+?>
